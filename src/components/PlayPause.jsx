5b64541194bba3eec14c9a9826e41c04b90a4f38
mane936 <53373350+mane936@users.noreply.github.com>
2023-05-08 13:07:29 +0200

    allow play

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,10 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle className="text-gray-300" onClick={handlePause} size={35} />
+  ) : (
+    <FaPlayCircle className="text-gray-300" onClick={handlePlay} size={35} />
+  );
 
 export default PlayPause;
--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,17 @@ import { Link } from 'react-router-dom';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
-  const activeSong = 'test';
+const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
@@ -16,9 +25,31 @@ const SongCard = ({ song, i }) => {
               : 'hidden'
           }`}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img src={song.images?.coverart} alt="song_img" />
+      </div>
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link
+            to={
+              song?.artists
+                ? `/artists/${song.artists[0]?.adamid}`
+                : '/top-artists'
+            }
+          >
+            {song.subtitle}
+          </Link>
+        </p>
       </div>
     </div>
   );
